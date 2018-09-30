7113ed5586d185897b6a760eda146d5370f2c07c
wesley.w <1093295006@qq.com>
2018-09-30 13:12:09 +0800

    add test data generator

---
+++ test/sqlGenerator.py
@@ -0,0 +1,30 @@
+import json
+tags = []
+infile = open('data/sock.json', 'r')
+data = json.load(infile, encoding='UTF-8')
+infile.close()
+fp = open("../docker/catalogue-db/data/dump1.sql", 'w')
+fp.truncate()
+temp_tags = []
+for sock in data:
+    sid = sock['id']
+    name = sock['name']
+    desc = sock['description']
+    price = str(sock['price'])
+    count = str(sock['count'])
+    url1 = sock['imageUrl'][0]
+    url2 = sock['imageUrl'][1]
+    temp_tags = temp_tags+sock['tag']
+    fp.write("INSERT INTO sock VALUES (\""+sid+"\",\""+name+"\",\""+desc+"\","+price+","+count+",\""+url1+"\","+"\""+url2+"\");"+'\n')
+for tag in temp_tags:
+    if tag not in tags:
+        tags.append(tag)
+for tag in tags:
+    fp.write("INSERT INTO tag VALUES (\"" + tag + "\");" + '\n')
+for sock in data:
+    sid=sock['id']
+    for tag in sock['tag']:
+        tid = str(tags.index(tag)+1)
+        fp.write("INSERT INTO sock_tag VALUES (\""+sid+"\",\""+tid+"\");"+'\n')
+
+fp.close()
